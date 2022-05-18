ad7f522a97525e303993765ebeb8449e72ef0f54
Omar Hussein <omarelwy@hotmail.com>
2022-05-18 19:58:36 +0200

    ✨ Connect to the database
    - Connect to the database
    - Add morgan for logs.
    - Create handler/index, models/index, and services/index files.

---
+++ src/database.ts
@@ -0,0 +1,5 @@
+import { Pool } from "pg";
+
+const client = new Pool();
+
+export default client;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,30 @@
-import express, { Request, Response } from "express";
+import "dotenv/config";
+
+import express, { Request, Response, NextFunction } from "express";
+import morgan from "morgan";
 
 import config from "./config";
 
-const app: express.Application = express();
+import dbClient from "./database";
+
+const app = express();
 
 const { host, port } = config;
 
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.get("/", (_req: Request, res: Response) => {
+app.get("/", (_req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/db", async (_req, res, next) => {
+  try {
+    const result = await dbClient.query("SELECT NOW()");
+    res.send(result.rows);
+  } catch (e) {
+    next(e);
+  }
 });
 
 app.listen(port, host, () => {
