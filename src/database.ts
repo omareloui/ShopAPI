e733a806becac9923f7ba3ac09d15af98fb92bef
Omar Hussein <omarelwy@hotmail.com>
2022-05-20 04:24:47 +0200

    🎨 Improve test and databases setup
    - Bring back the database.json file and configure it with env variables.
    - Set the database.ts file to work with .env file instead of the database.json file.

--- src/database.ts
+++ src/database.ts
@@ -2,20 +2,12 @@ import { Pool } from "pg";
 
 import config from "./config";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import databaseInfo from "../database.json";
-
-const currentDB = config.isTest ? "test" : "dev";
-
-const { database, host, password, port, user } = databaseInfo[currentDB];
+const database = config.isTest
+  ? process.env.PGDATABASE_TEST
+  : process.env.PGDATABASE;
 
 const client = new Pool({
   database,
-  host,
-  port,
-  user,
-  password,
 });
 
 export default client;
